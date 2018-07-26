fc0ff370b031c22ba31fb36a2a6a765d4ad1ed99
TinWu <yyuuix@gmail.com>
2018-07-26 12:05:52 +0800

    upt, index ok

---
+++ OutWeb/Controllers/ProductsController.cs
@@ -0,0 +1,13 @@
+﻿using OutWeb.Inc;
+using System.Web.Mvc;
+
+namespace OutWeb.Controllers
+{
+    public class ProductsController : WebUserController
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
