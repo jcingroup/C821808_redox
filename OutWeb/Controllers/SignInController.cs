714b1da7a0069a859890308b1e49a85b4c24ae8d
TinWu <yyuuix@gmail.com>
2018-07-27 14:32:50 +0800

    add 後台

---
+++ OutWeb/Controllers/SignInController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace OutWeb.Controllers
+{
+    public class SignInController : Controller
+    {
+        // GET: _SysAdm
+        public ActionResult Index()
+        {
+            return View("Login");
+        }
+    }
+}
