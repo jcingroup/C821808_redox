2035220e0d26b86e05b02a9676ba3e588d4cf2a8
shuanshuan030913 <skypainter59768@gmail.com>
2018-08-15 11:05:51 +0800

    Mail upt

---
+++ OutWeb/Controllers/MailController.cs
@@ -0,0 +1,18 @@
+﻿using OutWeb.Inc;
+using System.Web.Mvc;
+
+namespace OutWeb.Controllers
+{
+    public class MailController : WebUserController
+    {
+        public ActionResult Index()
+        {
+            return View("MsgMail");
+        }
+
+        public ActionResult MsgMail()
+        {
+            return View();
+        }
+    }
+}
